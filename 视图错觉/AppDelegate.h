9fe60a3b3a2d862ff2a9836e06702740a1528b45
54fanqie <2917545349@qq.com>
2016-08-15 10:50:29 +0800

    Initial Commit

---
+++ 视图错觉/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  视图错觉
+//
+//  Created by macmini2 on 16/8/15.
+//  Copyright © 2016年 emiage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 视图错觉/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  视图错觉
+//
+//  Created by macmini2 on 16/8/15.
+//  Copyright © 2016年 emiage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
